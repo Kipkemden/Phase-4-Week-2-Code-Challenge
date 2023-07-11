26af20501eb2f3d2baa69561ad1b89a2ce0b399c
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-07-11 18:55:56 +0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Clear existing data
+HeroPower.destroy_all
+Power.destroy_all
+Hero.destroy_all
+
+# Create Heroes
+kamala = Hero.create(name: 'Kamala Khan', super_name: 'Ms. Marvel')
+doreen = Hero.create(name: 'Doreen Green', super_name: 'Squirrel Girl')
+gwen = Hero.create(name: 'Gwen Stacy', super_name: 'Spider-Gwen')
+
+# Create Powers
+strength = Power.create(name: 'Super Strength', description: 'Gives the wielder super-human strengths')
+flight = Power.create(name: 'Flight', description: 'Gives the wielder the ability to fly through the skies at supersonic speed')
+
+# Create HeroPowers
+HeroPower.create(hero: kamala, power: strength, strength: 'Strong')
+HeroPower.create(hero: kamala, power: flight, strength: 'Average')
+HeroPower.create(hero: doreen, power: strength, strength: 'Average')
+HeroPower.create(hero: gwen, power: flight, strength: 'Weak')
+
+puts 'Seeding completed successfully!'
